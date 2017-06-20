7acd6a4145beb90324cfbc83d7d303f34cdab530
Przemysław Walkowski <przemyslaw.walkowski@bccgroup.com>
2017-06-20 14:27:33 +0200

    Gotowy ściagacz plików

--- SimpleWebDownloader/Program.cs
+++ SimpleWebDownloader/Program.cs
@@ -25,24 +25,43 @@ namespace SimpleWebDownloader
 
             var direcotry = new DirectoryInfo(args[1]);
 
+            var lineCount = File.ReadLines(args[0]).Count();
+
             var fileStream = new FileStream(args[0], FileMode.Open);
 
+            
 
             using (var sr = new StreamReader(fileStream))
             {
                 var line = "";
+                int currentLine = 1;
 
                 while ((line = sr.ReadLine()) != null)
                 {
-                    var uri = line.Replace("imgbox.com", "i.imgbox.com").Replace("http", "https");
+                    try
+                    {
+                        var uri = line.Replace("imgbox.com", "i.imgbox.com").Replace("http", "https");
 
-                    var fileName = line.Split('/').ToList().Last();
+                        var fileName = line.Split('/').ToList().Last();
 
-                    var result =  Client.GetByteArrayAsync(uri);
+                        Console.WriteLine($"{currentLine}/{lineCount} {line} "  );
 
-                    File.WriteAllBytes(Path.Combine(direcotry.FullName, $"{fileName}.jpg"), result.Result);
+                        var result = Client.GetByteArrayAsync(uri);
+
+                        File.WriteAllBytes(Path.Combine(direcotry.FullName, $"{fileName}.jpg"), result.Result);
+                    }
+                    catch (Exception e)
+                    {
+                        var logStream = new FileStream("log.txt", FileMode.OpenOrCreate);
+
+                        using (var sw = new StreamWriter(logStream))
+                        {
+                            sw.WriteLine($"{line} ------ {e.Message}");
+                        }
+                    }
+
+                    currentLine++;
                 }
-
             }
 
                 Console.WriteLine("SimpleWebDownloader");
